cdcbe43a70ad65f85e2d2eb466d40978672bd1ef
DSalischiker <diego@studiohakuna.com>
2023-02-08 19:25:38 -0300

    change position of box

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function Button(props) {
     <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
       <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]} size={[0.4, 0.1, 0.1]} {...props}>
         <Suspense fallback={null}>
-          <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+          <Text position={[0, 5, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
             Hello react-xr!
           </Text>
         </Suspense>
